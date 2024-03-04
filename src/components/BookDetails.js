e791f7bcfc2d1a43656ec741dd87fec5d070b467
sree1411 <sreekanthsree141122@gmail.com>
2024-03-04 10:58:32 +0530

    created alternative way of crud operation

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,94 +1,98 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 export const BookDetails = () => {
+  const [books, setBooks] = useState([]);
+  const [newBook, setNewBook] = useState({ title: '', author: '' });
+  const [show, setShow] = useState(false);
+  const [editedBook, setEditedBook] = useState(null);
 
- const [books, setBooks] = useState([])
- const [newBook, setNewBook] = useState({"title": "", "author":""})
- const [show, setshow] = useState(false)
- const [editedBook, setEditedBook] = useState(null)
+  useEffect(() => {
+    fetchBookInfo();
+  }, []);
 
+  const addBook = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:8000/posts', newBook);
+    fetchBookInfo();
+  };
 
-  useEffect(()=>{
-    bookInfo()
-  }, [])
+  const deleteBook = async (id) => {
+    await axios.delete(`http://localhost:8000/posts/${id}`);
+    fetchBookInfo();
+  };
 
+  const editBook = (book) => {
+    setEditedBook({ ...book });
+    setShow(true);
+  };
 
-function addBook(){
-    axios.post("http://localhost:8000/posts", newBook).then(()=>{
-        bookInfo()
-    })
-}
+  const updateBook = async (e) => {
+    e.preventDefault();
+    await axios.put(`http://localhost:8000/posts/${editedBook.id}`, editedBook);
+    setShow(false);
+    fetchBookInfo();
+  };
 
-
-
-function deleteBook(id){
-    axios.delete(`http://localhost:8000/posts/${id}`).then(()=>{
-        bookInfo()
-    })
-}
-
-function editBook(book){
-   setEditedBook({...book})
-   setshow(true)
-}
-
-
-function updatedBook(){
-    axios.put(`http://localhost:8000/posts/${editedBook.id}`, editedBook).then((res)=>{
-        bookInfo(res)
-    })
-}
-
-function bookInfo(){
-    axios.get("http://localhost:8000/posts").then((res)=>{
-        setBooks(res.data)
-    
-    })
-}
-
+  const fetchBookInfo = async () => {
+    const response = await axios.get('http://localhost:8000/posts');
+    setBooks(response.data);
+  };
 
   return (
-     <>
-     <div>BookDetails</div>
-
-      {
-        !show && (<div>
-            <input type="text" placeholder='enter the title' onChange={(e)=>{setNewBook({...newBook, title:e.target.value})}} />
-            <input type="text" placeholder='enter the author' onChange={(e)=>{setNewBook({...newBook, author:e.target.value})}} />
-            <button onClick={()=>{addBook()}}> Add book </button>
-       </div>)
-      }
-      {
-        show && (
-            <div>
-           <input type="text" placeholder='enter the title'  value={editedBook.title} onChange={(e)=>{setEditedBook({...editedBook, title:e.target.value})}} />
-           <input type="text" placeholder='enter the author'  value={editedBook.author}  onChange={(e)=>{setEditedBook({...editedBook, author:e.target.value})}} />
-           <button onClick={()=>{updatedBook()}}> updated book </button>
-              </div>
-        )
-      }
-      
-    
+    <>
+      {/* here i create the form and along with the  use the async await thing alternative approach
+      for the crud operation */}
 
 
+      <div>BookDetails</div>
 
+      {!show && (
+        <form onSubmit={addBook}>
+          <input
+            type="text"
+            placeholder="enter the title"
+            value={newBook.title}
+            onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="enter the author"
+            value={newBook.author}
+            onChange={(e) => setNewBook({ ...newBook, author: e.target.value })}
+          />
+          <button type="submit">Add book</button>
+        </form>
+      )}
 
+      {show && (
+        <form onSubmit={updateBook}>
+          <input
+            type="text"
+            placeholder="enter the title"
+            value={editedBook.title}
+            onChange={(e) => setEditedBook({ ...editedBook, title: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="enter the author"
+            value={editedBook.author}
+            onChange={(e) => setEditedBook({ ...editedBook, author: e.target.value })}
+          />
+          <button type="submit">Update book</button>
+        </form>
+      )}
 
-     {
-        books.length>0 && books.map((book)=>{
-           return <li>
-                   <p> Title : {book.title}</p>
-                   <p> Author :{book.author}</p>
-                   <button onClick={()=>{deleteBook(book.id)}}> delete </button>
-                   <button onClick={()=>{editBook(book)}}> edit </button>
-
-
-                </li>
-        })
-     }
-     
-     
-     </>
-  )
-}
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <p>Title: {book.title}</p>
+            <p>Author: {book.author}</p>
+            <button onClick={() => deleteBook(book.id)}>Delete</button>
+            <button onClick={() => editBook(book)}>Edit</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
